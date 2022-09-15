e42e53cc03c60aa93e1e87fe7e78bc30c4611c9c
IFPI <aluno@ifpi.edu.br>
2022-09-15 19:58:57 -0300

    java m

--- src/br/edu/ifpi/dominio/Aluno.java
+++ src/br/edu/ifpi/dominio/Aluno.java
@@ -1,8 +1,35 @@
 package br.edu.ifpi.dominio;
+
 import java.util.Date;
 
 public class Aluno {
-    public String nome;
-    public Date dataDeNascismento;
-    public String email;
+    private String nome;
+    private String email;
+    private Date dataDeNa;
+
+    public Aluno(String nome, String email, Date dataDeNa) {
+        this.nome = nome;
+        this.email = email;
+        this.dataDeNa = dataDeNa;
+    }
+
+    public String getnome() {
+        return nome;
+    }
+
+    public String getemail() {
+        return email;
+    }
+
+    public Date getdataDeNa() {
+        return dataDeNa;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno [dataDeNa=" + dataDeNa + ", email=" + email + ", nome=" + nome + "]";
+    }
+
+    
+
 }
